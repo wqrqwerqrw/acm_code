d5cff530f2d47377fb9485ce0ce58829167c344c
wqrqwerqrw <xueduanwei@126.com>
2019-05-15 08:28:41 +0800

    new

---
+++ cf/560-3/a.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+    > File Name: a.cpp
+    > Author: Wqr_
+    > Mail: xueduanwei@126.com 
+    > Created Time: 2019年05月15日 星期三 08时25分27秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+
+int main(){
+
+    return 0;
+}
