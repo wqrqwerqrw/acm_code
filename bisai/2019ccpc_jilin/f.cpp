fe748204f5485bb43a9789ed425faa8af380d5f3
wqrqwerqrw <xueduanwei@126.com>
2019-05-24 18:31:58 +0800

    new

---
+++ bisai/2019ccpc_jilin/f.cpp
@@ -0,0 +1,61 @@
+/*************************************************************************
+    > File Name: f.cpp
+    > Author: Wqr_
+    > Mail: xueduanwei@126.com 
+    > Created Time: 2019年05月23日 星期四 20时10分44秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+const int MAXN = 310;
+int mapp[MAXN][MAXN];
+int mapc[MAXN][MAXN];
+int n;
+int v = 0;
+void solve(){
+    for(int k = 0; k < n; k++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(mapc[i][j] < mapc[i][k] + mapc[k][j]){
+                    mapc[i][j] = mapc[i][k] + mapc[k][j];
+                }
+            }
+        }
+    }
+    int ans = v;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(mapp[i][j] > mapc[i][j]){
+                ans--;
+                mapp[i][j] = mapc[i][j];
+                mapp[j][i] = mapc[i][j];
+            }
+        }
+    }
+    cout << ans << endl;
+}
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> mapp[i][j];
+            mapc[i][j] = mapp[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(mapp[i][j] > 0) v++;
+        }
+    }
+    solve();
+    return 0;
+}
---
+++ test/java_t.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+    > File Name: java_t.cpp
+    > Author: Wqr_
+    > Mail: xueduanwei@126.com 
+    > Created Time: 2019年05月20日 星期一 22时21分54秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+
+int main(){
+
+    return 0;
+}
