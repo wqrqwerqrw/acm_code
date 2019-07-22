9a49fd5a1dd69e82917e9521dd4f65b6896bf7a8
wqrqwerqrw <xueduanwei@126.com>
2019-07-22 17:50:18 +0800

    new

--- luogu/1071.cpp
+++ luogu/1071.cpp
@@ -9,17 +9,29 @@ int main(){
     iofuck;
     string a, b, c;
     map<char, char> mapp;
+    set<char> sett;
     bool iff = 0;
     cin >> a >> b >> c;
+    for(auto i : b) sett.insert(i);
+    if(sett.size() != 26) iff = 1;
     if(a.length() != b.length()) iff = 1;
     for(int i = 0; i < a.length(); i++){
         auto it = mapp.find(a[i]);
-        if(it != mapp.end()) mapp[a[i]] = b[i];
-        else iff = 1;
+        if(it == mapp.end()) mapp[a[i]] = b[i];
+        else if(mapp[a[i]] != b[i]) iff = 1;
+    }
+    for(int i = 0; i < c.length(); i++){
+        auto it = mapp.find(c[i]);
+        if(it == mapp.end()){
+            iff = 1;
+            break;
+        }
     }
     if(iff) cout << "Failed" << endl;
     else{
-        for(int i = 0; i < c.length(); i++) cout << mapp[c[i]];
+        for(int i = 0; i < c.length(); i++) {
+            cout << mapp[c[i]];
+        }
     }
     cout << endl;
     return 0;
