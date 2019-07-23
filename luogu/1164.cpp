f9f68cced8685fd1eeb82d04397a9e796ec4c7e9
wqrqwerqrw <xueduanwei@126.com>
2019-07-23 17:53:38 +0800

    new

---
+++ luogu/1164.cpp
@@ -0,0 +1,12 @@
+// Author : Wqr_
+// Time : 2019年07月22日 星期一 20时39分16秒
+
+#include<bits/stdc++.h>
+#define iofuck std::ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+using namespace std;
+typedef long long ll;
+int main(){
+    int n, m;
+    cin >> n >> m;
+    return 0;
+}
