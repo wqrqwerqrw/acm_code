9374c637358dc5e75622a05bb95322f48225cbf5
wqrqwerqrw <wqrlink@outlook.com>
2019-09-09 22:46:16 +0800

    test

---
+++ gittest/gittest.cpp
@@ -0,0 +1,11 @@
+// Author : Wqr_
+// Time : 2019/9/9 18:57:39
+#include<bits/stdc++.h>
+#define iofuck std::ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define IN freopen("in.txt","r",stdin); 
+using namespace std;
+typedef long long ll;
+int main(){
+    cout << 1 << endl;
+    return 0;
+}
