38dd596688f2275232ca7d441b4d2e3df7d0ce9c
wqrqwerqrw <wqrlink@outlook.com>
2019-09-09 20:30:59 +0800

    m

--- dp/uva-12563.cpp
+++ dp/uva-12563.cpp
@@ -1,33 +1,38 @@
-/*************************************************************************
-    > File Name: uva-12563.cpp
-    > Author: Wqr_
-    > Mail: xueduanwei@126.com 
-    > Created Time: 2019年06月04日 星期二 19时46分46秒
- ************************************************************************/
+// Author : Wqr_
+// Time : 2019/9/9 18:57:39
+// 10ms..
 
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<string>
-#include<vector>
-#include<algorithm>
-#include<map>
-#include<queue>
-#include<cmath>
-#define iofuck std::ios::sync_with_stdio(false)
+#include<bits/stdc++.h>
+#define iofuck std::ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
 using namespace std;
 typedef long long ll;
-const int nmax = 100;
-const int jj = 678;
-int n;
+const int nmax = 50 + 5;
+const int tmax = 180 * 50 + 1000;
+int dp[nmax][tmax];
+int n, t;
 int main(){
-    iofuck;
-    int t;
-    cin >> t;
-    while(t--){
-        cin >> n;
+    int T;
+    cin >> T;
+    int kase = 0;
+    while(T--){
+        cin >> n >> t;
+        t--;
+        int ans = -1;
+        int len;
+        for(int i = 0; i <= t; i++) dp[0][i] = -1;
+        dp[0][0] = 0;
         for(int i = 1; i <= n; i++){
-
+            scanf("%d", &len);
+            for(int j = 0; j <= t; j++){
+                dp[i][j] = dp[i-1][j];
+                if(j >= len && dp[i-1][j-len] >= 0)
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-len] + 1);
+                ans = max(ans, dp[i][j]);
+           }
+        }
+        for(int i = t; i >= 0; i--) if(dp[n][i] == ans){
+            printf("Case %d: %d %d\n", ++kase, ans + 1, i + 678);
+            break;
         }
     }
     return 0;
