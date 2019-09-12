1f9b6b83614210d3ff5e4cfd49c8ac8b7c64b631
wqrqwerqrw <wqrlink@outlook.com>
2019-09-12 10:01:47 +0800

    9-12

--- mi/java30.java
+++ mi/java30.java
@@ -1,19 +1,7 @@
 
-import java.math.*;
-import java.util.*;
 
 public class java30 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BigInteger a = scanner.nextBigInteger();
-        String s = a.toString(2);
-        while(s.length() < 32){
-            s = "0" + s;
-        }
-        s = new StringBuffer(s).reverse().toString();
-        BigInteger b = new BigInteger(s, 2);
-        String out = b.toString(10);
-        System.out.println(out);
-        scanner.close();
+        System.out.println("test");
     }
 }
---
+++ test/java30.java
@@ -0,0 +1,8 @@
+import java.math.*;
+import java.util.*;
+
+public class java30 {
+    public static void main(String[] args) {
+        System.out.println("tees");
+    }
+}
