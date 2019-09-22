32fcd0f96037557c24fc247ee744e577d5a7fce3
wqrqwerqrw <wqrlink@outlook.com>
2019-09-22 11:54:15 +0800

    新系统

--- mi/java30.java
+++
@@ -1,7 +0,0 @@
-
-
-public class java30 {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
-}
--- test/java30.java
+++
@@ -1,8 +0,0 @@
-import java.math.*;
-import java.util.*;
-
-public class java30 {
-    public static void main(String[] args) {
-        System.out.println("tees");
-    }
-}
